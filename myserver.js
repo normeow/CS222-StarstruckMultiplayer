cc407118b948ce6e0c0e887697d40292ca7ae79d
normeow <rgdar@yandex.ru>
2017-07-04 18:41:16 +0300

    initial commit

---
+++ myserver.js
@@ -0,0 +1,32 @@
+ 
+var express = require('express')
+  , app = express(app)
+  , server = require('http').createServer(app);
+ 
+ 
+// serve static files from the current directory
+app.use(express.static(__dirname));
+
+
+//get EurecaServer class
+var Eureca = require('eureca.io');
+ 
+//create an instance of EurecaServer
+var eurecaServer = new Eureca.Server();
+ 
+//attach eureca.io to our http server
+eurecaServer.attach(server);
+
+//detect client connection
+eurecaServer.onConnect(function (conn) {    
+    console.log('New Client id=%s ', conn.id, conn.remoteAddress);
+
+});
+ 
+//detect client disconnection
+eurecaServer.onDisconnect(function (conn) {    
+
+});
+
+ 
+server.listen(8000);
